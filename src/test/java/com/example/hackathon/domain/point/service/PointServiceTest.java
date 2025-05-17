58568c21bf645ec786c16abc938f920cc50f8548
leejunnyeop <daffodil8620@naver.com>
2025-05-18 00:57:45 +0900

    test:

---
+++ src/test/java/com/example/hackathon/domain/point/service/PointServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.hackathon.domain.point.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class PointServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
