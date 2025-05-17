3dc9bba3b932f6db277d2256700592224a577c5f
leejunnyeop <daffodil8620@naver.com>
2025-05-17 18:20:17 +0900

    fix : ecr 저장 이름 오류

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-docker stop hackathon-server-s3 || true
-docker rm hackathon-server-s3 || true
+docker stop hackathon-ecr || true
+docker rm hackathon-ecr || true
 docker pull 365499268631.dkr.ecr.ap-northeast-2.amazonaws.com/hackathon-ecr:latest
 docker run -d --name instagram-server -p 8080:8080 365499268631.dkr.ecr.ap-northeast-2.amazonaws.com/hackathon-ecr:latest
 echo "--------------- 서버 배포 끝 -----------------"
