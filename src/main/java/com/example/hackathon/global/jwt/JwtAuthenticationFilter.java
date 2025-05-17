31949084043044892834e67755740e37f65071c2
Chris Jang <qiiopasd1@gmail.com>
2025-05-18 07:11:28 +0900

    fix: authorities 빈 값 등록

--- src/main/java/com/example/hackathon/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/hackathon/global/jwt/JwtAuthenticationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -47,7 +48,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         new UsernamePasswordAuthenticationToken(
                                 user,
                                 null,
-                                null
+                                Collections.emptyList()  // ✅ 반드시 authorities 포함
                         );
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
