ec7e08d631060d353f9231be2f19b7ef2536beac
Chris Jang <qiiopasd1@gmail.com>
2025-05-18 02:15:33 +0900

    feat: OAuth 주석 제거

--- src/main/java/com/example/hackathon/global/auth/service/KakaoOAuthService.java
+++ src/main/java/com/example/hackathon/global/auth/service/KakaoOAuthService.java
@@ -1,5 +1,7 @@
 package com.example.hackathon.global.auth.service;
 
+import com.example.hackathon.domain.user.entity.User;
+import com.example.hackathon.domain.user.repository.UserRepository;
 import com.example.hackathon.global.auth.client.KakaoOAuthClient;
 import com.example.hackathon.global.auth.dto.KakaoUserInfoDto;
 import com.example.hackathon.global.auth.dto.TokenResponseDto;
@@ -12,10 +14,16 @@ import org.springframework.stereotype.Service;
 public class KakaoOAuthService implements OAuthService {
 
     private final KakaoOAuthClient kakaoOAuthClient;
+    private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
-    public KakaoOAuthService(KakaoOAuthClient kakaoOAuthClient, JwtUtil jwtUtil) {
+    public KakaoOAuthService(
+            KakaoOAuthClient kakaoOAuthClient,
+            UserRepository userRepository,
+            JwtUtil jwtUtil
+    ) {
         this.kakaoOAuthClient = kakaoOAuthClient;
+        this.userRepository = userRepository;
         this.jwtUtil = jwtUtil;
     }
 
@@ -30,13 +38,11 @@ public class KakaoOAuthService implements OAuthService {
         log.info("userInfo : {}", userInfo);
 
         // 3. 회원 조회 또는 저장
-//        User user = userRepository.findByEmail(userInfo.getEmail())
-//                .orElseGet(() -> userRepository.save(userInfo.toEntity()));
+        User user = userRepository.findById(userInfo.getId())
+                .orElseGet(() -> userRepository.save(userInfo.toEntity()));
 
         // 4. JWT 토큰 발급
-//        return jwtUtil.generateToken(user.getEmail());
-        log.info("JWT TOKEN : {}", jwtUtil.generateToken(userInfo.getId()));
-        return "";
+        return jwtUtil.generateToken(user.getId());
     }
 }
 
