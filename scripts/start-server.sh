51a9d8a015be0cb99dc297d8e7f9d33c99c7724b
leejunnyeop <daffodil8620@naver.com>
2025-05-17 22:47:10 +0900

    feat : cicd 다시 구성

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-docker stop hackthon-ecr || true
-docker rm hackthon-ecr || true
-docker pull 365499268631.dkr.ecr.ap-northeast-2.amazonaws.com/hackthon-ecr:latest
-docker run -d --name hackthon-ecr -p 8080:8080 365499268631.dkr.ecr.ap-northeast-2.amazonaws.com/hackthon-ecr:latest
+docker stop hackathon-server || true
+docker rm hackathon-server || true
+docker pull 365499268631.dkr.ecr.ap-northeast-2.amazonaws.com/hackathon-server:latest
+docker run -d --name hackathon-server -p 8080:8080 365499268631.dkr.ecr.ap-northeast-2.amazonaws.com/hackathon-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
